dc938a568c250f93ff1e2bb97a0c591ec374ac3c
Osama Muhamed <63379624+U-sama@users.noreply.github.com>
2022-12-20 16:24:42 +0200

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.caption
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=150)
+    exerpt = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True, primary_key=True)
+    date = models.DateField(auto_now=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, related_name="posts", null=True, on_delete=models.SET_NULL)
+    tag = models.ManyToManyField(Tag)
--- blog/views.py
+++ blog/views.py
@@ -1,96 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datetime import date
+from .models import Post
 
 # Create your views here.
 all_posts = [
-    {
-        "slug" : "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Osama",
-        "date": date(2020, 7, 15),
-        "title": "Mountain Hiking",
-        "exerpt": "There is nothing loke the views and the montains you see in the forests and I wasn't prepared of what happend!",
-        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Minus deleniti quaerat blanditiis repellendus laudantium commodi possimus maiores illum,
-                     magni in quod, eaque odit placeat officia aliquam voluptas, autem nam ex?
-                     
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Minus deleniti quaerat blanditiis repellendus laudantium commodi possimus maiores illum,
-                     magni in quod, eaque odit placeat officia aliquam voluptas, autem nam ex?
-                     
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Minus deleniti quaerat blanditiis repellendus laudantium commodi possimus maiores illum,
-                     magni in quod, eaque odit placeat officia aliquam voluptas, autem nam ex?
-                     
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Minus deleniti quaerat blanditiis repellendus laudantium commodi possimus maiores illum,
-                     magni in quod, eaque odit placeat officia aliquam voluptas, autem nam ex?"""
 
-    },
-
-    {
-        "slug" : "Coding",
-        "image": "coding.png",
-        "author": "Osama",
-        "date": date(2022, 1, 20),
-        "title": "Clean Code",
-        "exerpt": "I will show you the best prctice youshould do to have your code clean and ready for real world deployment and be more rubost to changes.",
-        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Minus deleniti quaerat blanditiis repellendus laudantium commodi possimus maiores illum,
-                     magni in quod, eaque odit placeat officia aliquam voluptas, autem nam ex?
-                     
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Minus deleniti quaerat blanditiis repellendus laudantium commodi possimus maiores illum,
-                     magni in quod, eaque odit placeat officia aliquam voluptas, autem nam ex?
-                     
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Minus deleniti quaerat blanditiis repellendus laudantium commodi possimus maiores illum,
-                     magni in quod, eaque odit placeat officia aliquam voluptas, autem nam ex?
-                     
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Minus deleniti quaerat blanditiis repellendus laudantium commodi possimus maiores illum,
-                     magni in quod, eaque odit placeat officia aliquam voluptas, autem nam ex?"""
-
-    },
-
-    {
-        "slug" : "walking-in-the-woods",
-        "image": "woods.jpg",
-        "author": "Osama",
-        "date": date(2019, 6, 3),
-        "title": "Walking in the Woods",
-        "exerpt": "It's awesome to live in a diffrent way and try new things as we will walk thow how I manged to stay in the woods for more than 7 days without Masturbation ",
-        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Minus deleniti quaerat blanditiis repellendus laudantium commodi possimus maiores illum,
-                     magni in quod, eaque odit placeat officia aliquam voluptas, autem nam ex?
-                     
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Minus deleniti quaerat blanditiis repellendus laudantium commodi possimus maiores illum,
-                     magni in quod, eaque odit placeat officia aliquam voluptas, autem nam ex?
-                     
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Minus deleniti quaerat blanditiis repellendus laudantium commodi possimus maiores illum,
-                     magni in quod, eaque odit placeat officia aliquam voluptas, autem nam ex?
-                     
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Minus deleniti quaerat blanditiis repellendus laudantium commodi possimus maiores illum,
-                     magni in quod, eaque odit placeat officia aliquam voluptas, autem nam ex?"""
-
-    }
+       
 ]
 
 def get_date(post):
     return post['date']
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts,key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html",
     {"posts":latest_posts})
 
 def posts(request):
+    all_posts = Post.objects.all()
     return render(request, "blog/all-posts.html", {"all_posts":all_posts})
 
 def post_details(request,slug):
-    post = next(post for post in all_posts if post['slug'] == slug)
-    return render(request, 'blog/post-detail.html', {"post" : post})
+    post = get_object_or_404(Post, slug=slug)
+    #post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, 'blog/post-detail.html', {"post" : post, "all_tags":post.tag.all()})
